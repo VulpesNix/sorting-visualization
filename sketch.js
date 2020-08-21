2dea20f5eda9af902e7b6d2fadd4059faa63f460
Kayra <kayrabolat2004@gmail.com>
2020-08-21 23:09:40 +0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,72 @@
+let select;
+async function setup(){
+    createCanvas(600,600);
+    P = createP();
+    R = createP();
+    strokeWeight(1);
+    arr = new Array(width);
+    for(let i = 0;i < width;i++){
+            arr[i] = (min(i,height));
+            ptr.push(0);
+    }
+    R.html("Shuffling");
+    select = createSelect();
+    select.option("bubble");
+    select.option("quick");
+    select.option("selection");
+    select.option("pigeonhole");
+    select.selected("quick");
+    let sort = createButton("Sort");
+    let shuffle = createButton("Shuffle");
+    shuffle.mousePressed(reset);
+    sort.mousePressed(sorting);
+
+}
+
+function sorting(){
+    switch(select.value()){
+        case "bubble":
+            bubble(arr);
+            break;
+        case "quick":
+            quicksort(arr,0,arr.length-1);
+            break;
+        case "selection":
+            selection(arr);
+            break;
+        case "pigeonhole":
+            pigeon(arr);
+            break;
+    }
+}
+async function reset(){
+    compnum = 0;
+    exnum = 0;
+    for(let i = 0;i < width;i++){
+        arr[i] = (min(i,height));
+    }
+    shuf(arr);
+    compnum = 0;
+    exnum = 0;
+}
+function draw(){
+    P.html(int(frameRate()));
+    background(0,0,0);
+    for(let i = 0;i < width;i++){
+        if(ptr[i] === 1){
+            stroke(255,0,0);
+            strokeWeight(5);
+        }
+        else{
+            stroke(255,255,255);
+        }
+        line(i,height,i,height - arr[i]);
+        strokeWeight(1);
+
+
+    }
+         R.html("total exchange = "+(exnum)+"<br>"+"total comparison = "+compnum);
+}
+
+
+
---
+++ sorts.js
@@ -0,0 +1,114 @@
+let arr = [];
+let ptr = [];
+let P;
+let R;
+let exnum = 0;
+let compnum = 0;
+async function swap(arr,x,y){
+    exnum++;
+    let temp;
+    temp = arr[x]
+    arr[x] = arr[y];
+    arr[y] = temp;
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function shuf(arr){
+    for(let i = 0;i < 5000;i++){
+        let r = floor(random(arr.length-1));
+        let r1 = floor(random(arr.length-1));
+        await swap(arr,r,r1);
+    }
+    shuff = true
+}
+
+
+async function bubble(arr){
+    for(let i = arr.length;i >= 0;i--){
+        ptr[i] = 1;
+        for(let j = 0;j < i-1;j++){
+            compnum++;
+            if(arr[j]>arr[j+1]){
+               await swap(arr,j,j+1);
+            }
+        }await sleep(3);
+        ptr[i] = 0;
+    }
+}
+async function partion(arr,start,end){
+    let r = end;
+    let pivot = arr[r];
+    ptr[r] = 1;
+    let index = start;
+    for(let i = start;i<end;i++){
+        if(arr[i] < pivot){
+            compnum++;
+            await swap(arr,i,index);
+            await sleep(1);
+            index++;
+        }
+    } 
+    await swap(arr,r,index);
+    ptr[r] = 0;
+    return index;
+}
+async function quicksort(arr,start,end){
+    if(start >= end){
+        compnum++;
+        return;
+    }
+    let index = await partion(arr,start,end);
+    await Promise.all([
+        quicksort(arr, start, index - 1),
+        quicksort(arr, index + 1, end)
+      ]);
+}
+async function pigeon(arr){
+    let max = arr[0];
+    let min = arr[0]
+    for(let i = 1;i < arr.length;i++){
+        if(arr[i] > max){
+            max = arr[i];
+            ptr[i] = 1;
+        }
+        else if(arr[i] < min){
+            min = arr[i];
+            ptr[i] = 1;
+        }
+        await sleep(1);
+        ptr[i] = 0;
+        compnum += 2;
+    }
+    let holes = [];
+    for(let i = 0;i < max-min+1;i++){
+        holes[i] = [];
+    }
+    for(let i = 0;i < arr.length;i++){
+        holes[arr[i]-min].push(arr[i]);
+    }
+    for(let i = 1;i< holes.length;i++){
+        holes[0].push(holes[i]);
+    }
+    for(let i = 0;i < arr.length;i++){
+        exnum++;
+        arr[i] = holes[0][i];
+        await sleep(1);
+    }
+}
+async function selection(arr){
+    let minindex;
+    for(let i = 0;i<arr.length-1;i++){
+        minindex = i
+       for(let j = i+1;j < arr.length;j++){
+            if(arr[j]<arr[minindex]){
+               min = arr[j];
+               minindex = j;
+           }
+       }
+       await swap(arr,i,minindex);
+       ptr[minindex] = 1;
+       await sleep(1);
+       ptr[minindex] = 0;
+   }
+}
