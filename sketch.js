eecc8c558dc77e2124ead2abc8b0a9edcc1db658
VulpesNix <69347265+VulpesNix@users.noreply.github.com>
2020-08-25 17:00:51 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,45 @@
 // Kayra BOLAT
 let select;
-async function setup(){
-    createCanvas(600,600);
+let vis;
+let max = 2048;
+function setup(){
+    createCanvas(1200,600,P2D);
     P = createP();
     R = createP();
     strokeWeight(1);
-    arr = new Array(width);
-    for(let i = 0;i < width;i++){
-            arr[i] = (min(i,height));
+    arr = new Array(max);
+    for(let i = 0;i < max;i++){
+            arr[i] = i;
             ptr.push(0);
     }
+    let sort = createButton("Sort");
+    let shuffle = createButton("Shuffle");
     R.html("Shuffling");
     select = createSelect();
+    select.option("insertion");
     select.option("bubble");
     select.option("quick");
     select.option("selection");
     select.option("pigeonhole");
     select.selected("quick");
-    let sort = createButton("Sort");
-    let shuffle = createButton("Shuffle");
+    vis = createSelect();
+    vis.option("line");
+    vis.option("dots");
+    vis.option("spiral");
+    vis.option("color circle");
+    vis.option("color lines");
+    vis.selected("line");    
     shuffle.mousePressed(reset);
     sort.mousePressed(sorting);
-
 }
 
 function sorting(){
     switch(select.value()){
         case "bubble":
-            bubble(arr);
+            comb(arr);
             break;
         case "quick":
-            quicksort(arr,0,arr.length-1);
+            quicksort(arr,0,max-1);
             break;
         case "selection":
             selection(arr);
@@ -38,36 +47,97 @@ function sorting(){
         case "pigeonhole":
             pigeon(arr);
             break;
+        case "insertion":
+            insertion(arr);           
     }
 }
 async function reset(){
     compnum = 0;
     exnum = 0;
-    for(let i = 0;i < width;i++){
-        arr[i] = (min(i,height));
+    for(let i = 0;i < max;i++){
+        arr[i] = i;
     }
     shuf(arr);
     compnum = 0;
     exnum = 0;
 }
-function draw(){
+function draw(){;
     P.html(int(frameRate()));
     background(0,0,0);
-    for(let i = 0;i < width;i++){
-        if(ptr[i] === 1){
-            stroke(255,0,0);
-            strokeWeight(5);
-        }
-        else{
-            stroke(255,255,255);
-        }
-        line(i,height,i,height - arr[i]);
-        strokeWeight(1);
-
-
-    }
          R.html("total exchange = "+(exnum)+"<br>"+"total comparison = "+compnum);
+    switch(vis.value()){
+        case "line":
+            linegraph(arr);
+            break;
+        case "dots":
+            dots(arr);
+            break;
+        case "color circle":
+            colorcircle(arr);
+            break;
+        case "color lines":
+            colored(arr);
+            break;
+        case "spiral":
+            spiral(arr);
+            break;
+    }
 }
 
+function linegraph(){
+    stroke(255,255,255);
+    for(let i = 0;i < arr.length;i++){
+        let x = map(i,0,arr.length,0,width);
+        let y = map(arr[i],0,max,0,height);
+        y = height-y;
+        line(x,y,x,height);
+    }
 
-
+}
+function colored(){
+    colorMode(HSB);
+    for(let i = 0;i < arr.length;i++){
+        let x = map(i,0,arr.length,0,width);
+        let y = map(arr[i],0,max,0,height);
+        let c = map(y,0,height,0,360);
+        stroke(c,255,255);
+        y = height-y;
+        line(x,y,x,height);
+    }
+}
+function spiral(){
+    angleMode(DEGREES);
+    translate(width/2,height/2);
+    stroke(255);
+    for(let i = 0;i < arr.length;i++){
+        let theta = map(i,0,max,-90,5*360-90)
+        let ro = map(arr[i],0,max,0,height/2-10);
+        let x = ro * cos(theta);
+        let y = ro * sin(theta);
+        point(x,y);
+    }
+}
+function dots(){
+    stroke(255);
+    strokeWeight(2);
+      for(let i = 0;i < arr.length;i++){
+          let x = map(arr[i],0,max,0,width);
+          let y = map(i,0,max,0,height);
+          point(x,height-y);
+      }
+}
+function colorcircle(){
+    angleMode(DEGREES);
+    colorMode(HSB);
+    translate(width/2,height/2);
+    for(let i = 0;i < arr.length;i++){
+        let ro = floor(height/2) - 10;
+        let theta = map(i,0,max,-90,270)
+        let hu = map(arr[i],0,max,0,360);
+        let x = ro * cos(theta);
+        let y = ro * sin(theta);
+        stroke(hu,255,255);
+        
+        line(0,0,x,y);
+    }
+}
--- sorts.js
+++ sorts.js
@@ -33,12 +33,43 @@ async function bubble(arr){
             if(arr[j]>arr[j+1]){
                await swap(arr,j,j+1);
             }
-        }await sleep(3);
+        }
+        if(i%5 < 1){
+            await sleep(1);
+        }
         ptr[i] = 0;
     }
 }
+async function hoare(arr,start,end){
+    let q = floor(random(start,end));
+    await swap(arr,start,q);
+    let pivot = arr[start];
+    i = start-1;
+    j = end+1;
+    let ctr = 0;
+    while(true){
+        ctr++;
+        i+=1;
+        while(arr[i] < pivot){
+            i+=1;
+        }
+        j-=1;
+        while(arr[j] > pivot){
+            j-=1;
+        }
+        if(i>= j){
+            return j;
+        }
+        swap(arr,i,j);
+        if(ctr%5 < 1){
+            await sleep(1);
+        }
+    }
+}
 async function partion(arr,start,end){
+    let q = floor(random(start,end));
     let r = end;
+    await swap(arr,r,q);
     let pivot = arr[r];
     ptr[r] = 1;
     let index = start;
@@ -46,10 +77,12 @@ async function partion(arr,start,end){
         if(arr[i] < pivot){
             compnum++;
             await swap(arr,i,index);
-            await sleep(1);
-            index++;
+            index++;        
         }
-    } 
+        if(i%10 < 1){
+            await sleep(1);
+        }
+    }
     await swap(arr,r,index);
     ptr[r] = 0;
     return index;
@@ -59,11 +92,13 @@ async function quicksort(arr,start,end){
         compnum++;
         return;
     }
-    let index = await partion(arr,start,end);
-    await Promise.all([
+    let index = await hoare(arr,start,end);
+    /*await Promise.all([
         quicksort(arr, start, index - 1),
         quicksort(arr, index + 1, end)
-      ]);
+      ]);*/
+    await quicksort(arr, start, index);
+    await quicksort(arr, index + 1, end);
 }
 async function pigeon(arr){
     let max = arr[0];
@@ -77,7 +112,6 @@ async function pigeon(arr){
             min = arr[i];
             ptr[i] = 1;
         }
-        await sleep(1);
         ptr[i] = 0;
         compnum += 2;
     }
@@ -91,10 +125,13 @@ async function pigeon(arr){
     for(let i = 1;i< holes.length;i++){
         holes[0].push(holes[i]);
     }
+    
     for(let i = 0;i < arr.length;i++){
-        exnum++;
-        arr[i] = holes[0][i];
-        await sleep(1);
+            exnum++;
+            arr[i] = holes[0][i];
+            if(i%5 < 1){
+                await sleep(1);
+            }
     }
 }
 async function selection(arr){
@@ -109,7 +146,75 @@ async function selection(arr){
        }
        await swap(arr,i,minindex);
        ptr[minindex] = 1;
-       await sleep(1);
+       if(i%5 < 1){
+        await sleep(1);
+        }
        ptr[minindex] = 0;
    }
+}
+async function insertion(arr){
+    let index = 1;
+    for(let i = 1;i < arr.length;i++){
+        index = i;
+        while(arr[index]<arr[index-1]){
+            ptr[index] = 1;
+            await swap(arr,index,index-1);
+            ptr[index] = 0;
+            index--;
+            compnum++;    
+        }
+        if(i%5 < 1){
+            await sleep(1);
+        }
+    }
+}
+async function MergeSort(arr,l,r){
+    let m = r-l;
+    if(l > r ){
+        return;
+    }
+    await MergeSort(arr,l,m);
+    await MergeSort(arr,middle+1,r);
+    await merge(arr,l,m,r);
+}
+async function merge(arr,l,m,r){
+    let left = [];
+    let right = [];
+    let merged = [];
+    for(let i = 0;i < m-l+1;i++){
+        l[i] = arr[l+i];
+    }
+    for(j = 0;j < r-m;j++){
+        right[j] = arr[m+1+j];
+    }
+    let i = 0;
+    let j = 0;
+    let k = 1;
+
+}
+/*async function radix(arr){
+    let buckets = [];
+    let count = 0;
+    i = 10;
+    while(buckets[0].length < arr.length){
+        for(let j = 0; j< arr.length;j++){
+            buc
+        }
+    }
+
+}*/
+
+async function comb(arr){
+    for(let i = arr.length;i > 0;i--){
+        for(let j = 0;j < arr.length-i;j++){
+            if(arr[j] > arr[j+i]){
+                await swap(arr,j,i+j);
+            }
+
+        }            
+        /*if(i%5 < 1){
+            await sleep(1);
+        }*/
+        await sleep(1);
+    }
 }
